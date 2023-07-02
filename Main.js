91a45bd96c9ec099df5d3a52086d1ed15f20b17f
leonemunyao <leonemunyao@gmail.com>
2023-07-02 16:19:51 +0300

    Changes

--- Main.js
+++ Main.js
@@ -1,43 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            product: "T-Shirts",
-            brand: "Adidas",
-            selectedVariant: 0,
-            details: ["60% cotton", "20% wool", "20% polyester"],
-            // sizes: ['S', 'M', 'L', 'XL'],
-            variants: [
-                { id: 3340, color: "red", image: "./Assets/Images/3.jpeg", quantity: 40 },
-                { id: 3341, color: "black", image: "./Assets/Images/2.jpeg" , quantity: 0 },
-                { id: 3342, color: "grey", image: "./Assets/Images/7.jpeg", quantity: 60 }
-            ],
-            
+            cart: 0
         }
     },
 
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image 
-        },
-        InStock() {
-            return this.variants[this.selectedVariant].quantity 
-        }  
+        
     }
+   
 }) 
